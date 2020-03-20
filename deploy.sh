a5aba2c0a8081909a494b1d46992f310ff85cf92
hzx <871577942@qq.com>
2020-03-21 00:46:56 +0800

    fix: add permison to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,9 @@
 kill -9 $(pgrep webserver)
 cd ~/devops/
 git pull https://github.com/thinkinghere/devops.git
+cd deployserver/
+chmod +x deployserver
+cd ../
 cd webserver/
+chmod +x webserver
 ./webserver $
