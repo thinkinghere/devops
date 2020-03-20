70e93ae527ae79a34d2c183d9ec7c32e6d78c97d
hzx <871577942@qq.com>
2020-03-21 00:51:34 +0800

    test

--- webserver/main.go
+++ webserver/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func firstPage(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "<h1>hello devops first page</h1>")
+	io.WriteString(w, "<h1>hello devops first page1</h1>")
 }
 
 func main() {
