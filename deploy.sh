29d237a001c20c039e0ee0decb14585a75f50e26
hzx <871577942@qq.com>
2020-03-21 11:52:53 +0800

    change webserver

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,6 @@
 kill -9 $(pgrep webserver)
 cd ~/devops/
 git pull https://github.com/thinkinghere/devops.git
-cd deployserver/
-chmod +x deployserver
-cd ../
 cd webserver/
 chmod +x webserver
 ./webserver $
